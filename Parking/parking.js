b0b714536821616e03f91ebe3c344e237ebab890
Katie <katiemkov@gmail.com>
2025-11-18 11:55:28 -0800

    parking

---
+++ Parking/parking.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundImage = "url('parkpic.jpg')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+    
+document.getElementById('background-audio').loop = true;
+
+document.getElementById('background-audio').play();   
