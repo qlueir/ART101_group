0731d49e3f4b210b181cb77a66d013ec6eaea160
qlueir <qarqizhhh8@gmail.com>
2025-11-20 17:17:10 -0800

    hh

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ let elevator = new Audio ('ADD SOME ELEVATOR MUSIC.mp3')
 elevator.play('ADD SOME ELEVATOR MUSIC.mp3')
 elevator.loop = true
 
-/*
-$(".buttons").click(function() {
-    e.preventDefault();
-    let url = $(this).attr("href");
-    ding.play();
-    elevator.pause();
-    });
-*/
-    $(window).on("pageshow", function() {
+
+$(window).on("pageshow", function() {
         $("body").removeClass("fade-out");
     });
 
