2695798f7a515d2a733a0bb235ba5783bf6968fe
qlueir <qarqizhhh8@gmail.com>
2025-11-20 17:04:49 -0800

    elevator button transition fades to black

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//when button clicked, play ding noise and change img to "transition"
+$(document).ready(function() {
+
+let ding = new Audio ('image/ding.mp3')
+let elevator = new Audio ('ADD SOME ELEVATOR MUSIC.mp3')
+elevator.play('ADD SOME ELEVATOR MUSIC.mp3')
+elevator.loop = true
+
+/*
+$(".buttons").click(function() {
+    e.preventDefault();
+    let url = $(this).attr("href");
+    ding.play();
+    elevator.pause();
+    });
+*/
+    $(window).on("pageshow", function() {
+        $("body").removeClass("fade-out");
+    });
+
+    $(".dingdong").click(function() {
+        ding.play();
+        elevator.pause();
+
+        let url = $(this).data("url");
+        if (!url) return;
+
+        $("body").addClass("fade-out");
+
+        setTimeout(function() {
+           window.location.href = url;
+        }, 1500);
+        });
+});
