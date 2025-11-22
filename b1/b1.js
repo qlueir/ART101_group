91bb25cc901f3072bb238d19e3bc604a0a2a0a48
Katie <katiemkov@gmail.com>
2025-11-22 00:35:02 -0800

    im tired

--- b1/b1.js
+++ b1/b1.js
@@ -2,3 +2,6 @@ let audio = document.getElementById('background-audio');
 audio.loop = true;
 audio.muted = false;
 audio.play();
+audio.volume = 0.3;
+
+
--- floor2/2.js
+++ floor2/2.js
@@ -1,21 +1,36 @@
 document.getElementById('background-audio').play();
 document.getElementById('background-audio').loop = true;
 document.getElementById('background-audio').muted = false;
-document.getElementById('background-audio').volume = 0.5;
+document.getElementById('background-audio').volume = 0.3;
 
-  const noButton = document.getElementById('no');
+var noButton = document.getElementById('no');
 
-  noButton.addEventListener('click', () => {
+noButton.addEventListener('click', function () {
     window.location.href = '../index.html';
-  });
+});
 
-   const dogGif = document.getElementById('dogfloor2gif');
+var dogGif = document.getElementById('dogfloor2gif');
 
-  const sounds = ['dogwoof.mp3', 'dogcry.mp3', 'dogbark.mp3'];
+var sounds = ['dogwoof.mp3', 'dogcry.mp3', 'dogbark.mp3'];
 
-  dogGif.addEventListener('click', () => {
-    const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+dogGif.addEventListener('click', function () {
+    var randomIndex = Math.floor(Math.random() * sounds.length);
+    var randomSound = sounds[randomIndex];
 
-    const audio = new Audio(randomSound);
+    var audio = new Audio(randomSound);
     audio.play();
-  });
+});
+
+var yesButton = document.getElementById('yes');
+
+var messageBox = document.getElementById('message');
+
+yesButton.addEventListener('click', function () {
+    messageBox.textContent = "You have decided to stay... enjoy!";
+});
+messageBox.classList.add('zoom-in');
+
+yesButton.addEventListener('click', function () {
+    messageBox.textContent = "You have decided to stay... enjoy!";
+    messageBox.classList.add('show');
+});
