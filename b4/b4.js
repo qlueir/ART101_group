a5bb5aec2eec9cfcb29d46d560d101a7afcf0470
jerica7875 <66146329+jerica7875@users.noreply.github.com>
2025-11-24 00:03:03 -0800

    jericas b4

--- b4/b4.js
+++ b4/b4.js
@@ -1,25 +1,49 @@
+$(document).ready(function () {
+    $("#mirror").click(function () {
+        $("#image-container").html("<img width='3000' src='images/b4 pt2.jpg'>");
+        $("#image-container").fadeIn ("slow")
+        $("#mirror").fadeOut (400);
+        $("#arrow").fadeOut(400);
+        let audio = document.getElementById('music');
+        audio.loop = true;
+        audio.muted = false;
+        audio.play();
 
-$("#mirror").click(function(){
- 
- $("#mirror").append("<img width=3000  src='images/b4 pt2.jpg'>");
-})
+    });
+});
+//
+//var roller = getRandomNumber(1, 6);
+function rollDice(roll_value) {
+  const dice = [...document.querySelectorAll(".die-list")];
+
+  dice.forEach((die) => {
+    toggleClasses(die);
+
+    die.dataset.roll = roll_value;
+  });
+}
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//document.getElementById("roll-button").addEventListener("click", rollDice);
+var dice = document.getElementById("cube");
+dice.onclick = function () {
+  var roll_value = getRandomNumber(1, 6);
+  rollDice(roll_value);
+  
+};
+
+
 
 $("#arrow").animate ({top: '11px'}); ({bottom: '20px'})
 
-var elem = document.getElementById("demo");
-  var useMe = ["right", "down", "left", "up"];
-  var interval;
 
-  function arrowmove() {
-  var i = 0;
-    elem.className = useMe[i];
-    interval = setInterval(function(){ 
-            if(i < 4){
-            i = i + 1;
-            elem.className = useMe[i];
-            }
-            else{
-                clearInterval(interval);
-            }
-        }, 1000);
-}
+
