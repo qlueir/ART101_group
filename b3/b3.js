2d683f8b8fa04b8074bb8e8300059ebe275fadd6
Debra Kuang <dekuang2@gmail.com>
2025-11-25 11:31:59 -0800

    functional buttons
    made button to go back to index (prevents softlock), and worked on mazeGenerate function

--- b3/b3.js
+++ b3/b3.js
@@ -3,7 +3,7 @@ const mazeImages = []
 
 let gameState = {
     current: "",
-
+    location: "a2"
 }
 
 function Room(id, setImage){
@@ -41,25 +41,59 @@ function randomMaze(){
     return currentMaze
 }
 
-function getMazeLayout(mazeLayout){
+function getMazeLayout(mazePar){
     let mazeMap = [];
-    if (mazeLayout == "layout1"){
-        return
-    } else if (mazeLayout == "layout2"){
+    if (mazePar == "layout1"){
+        mazeMap =
+        ["0010", "0110", "0111", "0110", "0100",
+         "0011", "0110", "1100", "0010", "1101",
+         "1001", "0011", "0100", "0011", "1100",
+         "1010", "1010", "0110", "1110", "0101",
+         "1010", "0110", "0101", "0010", "1100"
+        ];
+        
+    } else if (mazePar == "layout2"){
         return
     } else {
         return
     }
+    return mazeMap;
 }
 
 function mazeGenerate(){
-    
+    let mazeLayout = getMazeLayout();
+    debugger;
 
-    for (i=0; i<mazeArray.length(); i++){
-        
+    //for every item in mazeLayout, should be 25 for every cell of the maze
+    for (i=0; i<mazeLayout.length(); i++){
+
+        //each item is a string, so iterate through every character in the item (4 times)
+        for (j=0; j<mazeLayout[i].length(); j++){
+
+            //split the selected item into separate array of 4
+            //preserving the original array data
+            let tempvar = mazeLayout[i].split();
+            debugger;
+            //create another variable to retain result
+            let exitvar = [];
+
+            //iterate through tempvar 
+            for (k=0; k<tempvar.length(); k++){
+
+                //checking if there is or is not an exit accordingly
+                if (tempvar[k] == "0"){
+                    exitvar.push(false);
+                } else {
+                    exitvar.push(true)
+                }
+            }
+        }
+
+        mazeArray[i].exits = exitvar;
     }
 }
 
-$("button").click(function(){
+$("#start").click(function(){
+    $("#start").hide();
     mazeInitialize();
-})
+});
