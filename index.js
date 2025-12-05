88d304e736ba62674a82bd8d5ce8ef45f553dc15
qlueir <qarqizhhh8@gmail.com>
2025-12-04 17:48:30 -0800

    g floor

--- index.js
+++ index.js
@@ -32,6 +32,28 @@ $(document).ready(function() {
         });
     });
 
+    //===================TITLE=========================
+    $(window).on("pageshow", function () {
+        $("body").removeClass("fade-out");
+    });
+
+    var closeSound = document.getElementById("close-sound");
+
+    $("#start-button").on("click", function () {
+
+        closeSound.currentTime = 0;
+        closeSound.volume = 1;
+
+        closeSound.play().catch(function (err) {
+            console.log("AUDIO ERROR:", err);
+        });
+
+        $("body").addClass("fade-out");
+
+        setTimeout(function () {
+            window.location.href = "index.html";
+        }, 1500);
+    });
 
     //=====================div page show + hide===================
     //groundfloor button --> floor 1
