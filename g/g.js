4b6689077581dfd18e4ebb8807c2a49e3b336d65
akaarons <akaarons@ucsc.edu>
2025-12-04 22:55:12 -0800

    god save me

--- g/g.js
+++ g/g.js
@@ -3,7 +3,7 @@ function makeImage (imageName) {
 $("body").append("<img width=200 src='hell.png'>");
    }
  function makeImage2 (imageName) {
-$("body").append("<img width=500 src='trolleyproblem.png'>");
+$("body").append("<img width=400 src='trolleyProblem.png'>");
    }
 
  $("#hell").click(function(){
@@ -15,10 +15,20 @@ makeImage ("hell");
     $("body").append("<button id='trolleyProblem'>")
     $("#trolleyProblem").html(trolleyProblem);
  }
+ });
 
  $("#trolleyProblem").click(function(){
-    makeImage2("trolleyproblem");
- });
- 
+    $("#trolleyProblem").html ("Wuh oh, looks like you have a dillemma! Will you pull the lever to save the five people but sacrifice one?");
+makeImage2("trolleyProblem");
 
-});
+    makeButton2 ("Pull Lever");
+   function makeButton2 (lever) {
+    $("body").append("<button id='lever'>")
+    $("#lever").html(lever);
+   }
+    makeButton3 ("Do Nothing");
+   function makeButton3 (lever2) {
+    $("body").append("<button id='lever2'>")
+    $("#lever2").html(lever2);
+  }
+ });
