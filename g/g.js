3f98dc09f5d2cdc204b3cbcf9ecb7ae7809117a1
akaarons <akaarons@ucsc.edu>
2025-12-04 18:39:19 -0800

    gg

--- g/g.js
+++ g/g.js
@@ -1,20 +1,24 @@
 
-
 function makeImage (imageName) {
 $("body").append("<img width=200 src='hell.png'>");
+   }
+ function makeImage2 (imageName) {
+$("body").append("<img width=500 src='trolleyproblem.png'>");
    }
 
  $("#hell").click(function(){
     $("#hell").html ("Whoops! Wrong button :)" );
 makeImage ("hell");
 
-
-
  makeButton("Here, click this one instead!");
-
- function makeButton (trollyProblem) {
-    $("body").append("<button id='trollyProblem'>")
-    $("#trollyProblem").html(trollyProblem);
+  function makeButton (trolleyProblem) {
+    $("body").append("<button id='trolleyProblem'>")
+    $("#trolleyProblem").html(trolleyProblem);
  }
+
+ $("#trolleyProblem").click(function(){
+    makeImage2("trolleyproblem");
+ });
+ 
 
 });
