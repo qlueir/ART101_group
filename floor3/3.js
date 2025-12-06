fdf63e073bdd1b734ec3674f10abce3eb0252321
akaarons <akaarons@ucsc.edu>
2025-12-06 13:34:44 -0800

    floor 3

---
+++ floor3/3.js
@@ -0,0 +1,27 @@
+$("#creepyFace").click(function() {
+  $("#status").text("AAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆ");
+  $("body").css("background", "red");
+});
+
+$("#creepyFace").hover(
+  function() { $("#status").text("STAÆY ÆWAYYY"); },
+  function() { $("#status").text("IT BURNS IT BURNSITBURNSITBURNSITB"); }
+);
+
+
+$("#creepyFace").dblclick(function() {
+  $("#status").text("STOOŒŒŒŒŒŒŒOOOOOŒŒOOOOOOP!!!");
+  $("#creepyFace").css("transform", "scale(1.2)");
+});
+
+// $(document).keydown(function(event) {
+//   $("#status").text("You pressed: " + event.key);
+// });
+
+// $(document).mousemove(function(event) {
+//   $("#creature").css({
+//     left: event.pageX - 60,
+//     top: event.pageY - 60,
+//     position: "absolute"
+//   });
+// });
