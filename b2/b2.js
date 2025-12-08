6baf0f5249e62e0524054154b42de498597d7329
polbrown-cmyk <porterleebrown@gmail.com>
2025-12-08 15:23:11 -0800

    h

--- b2/b2.js
+++ b2/b2.js
@@ -5,3 +5,7 @@ $('#button').on('click', function() {
     $('#image').css('transform', 'rotate(' + angle + 'deg)');
 });
 
+var HTMLcontent = "<div class=\"buttons\"> <a href=\"../endingscreen.html\"><button id=yes> YES </button> </a> <a href=\"../index.html\"><button id=no> NO </button> </a></div>";
+$('#yes').on('click', function() {
+    window.location.href = "../endingscreen.html";
+});
