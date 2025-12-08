c0d2cea69a7cb408cd313df494cef2d4634e40ef
qlueir <qarqizhhh8@gmail.com>
2025-12-08 15:47:59 -0800

    d

--- endingscreen.js
+++ endingscreen.js
@@ -1,6 +1,5 @@
-
 var exitButton = document.getElementById("exit-button");
 
 exitButton.addEventListener("click", function() {
-    window.location.href = "../index.html";
+    window.location.href = "../index.html?fromFloor=1";
 });
